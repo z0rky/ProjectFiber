63d18f0256986c88a3cf34dc3b8128705899b6ba
Koen <zorkmeyers@gmail.com>
2017-11-26 14:12:44 +0100

    mvc locatie + navbar dropdown

---
+++ Eindwerk2018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eindwerk2018.MvcApplication" Language="C#" %>
---
+++ Eindwerk2018/Models/LocatieModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Eindwerk2018.Models
+{
+    public class LocatieModel
+    {
+        public LocatieModel()
+        {
+        }
+
+        public String LocatieNaam
+        {
+            get;
+            set;
+        }
+        public String GpsLong
+        {
+            get;
+            set;
+        }
+		
+        public String GpsLat
+        {
+            get;
+            set;
+        }
+
+        public String LocatieCode
+        {
+            get;
+            set;
+        }
+
+        public Boolean LocatieInfrabel
+        {
+            get;
+            set;
+        }
+
+        public Int16 LocatieType
+        {
+            get;
+            set;
+        }
+   
+    }
+}
