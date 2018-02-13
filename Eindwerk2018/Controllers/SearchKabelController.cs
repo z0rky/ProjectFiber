a21db3cf5c013a732a607ba011061bab53d8c77b
Unknown <koenmeyers81@gmail.com>
2018-02-13 22:23:30 +0100

    SearchKabel

---
+++ Eindwerk2018/Controllers/SearchKabelController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eindwerk2018.Controllers
+{
+    public class SearchKabelController : Controller
+    {
+        // GET: SearchKabel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        public ActionResult SearchReference()
+        {
+            return View("SearchKabelReference");
+        }
+    }
+}
--- Eindwerk2018/ViewModels/Search/SearchKabelViewModel.cs
+++ Eindwerk2018/ViewModels/Search/SearchKabelViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Eindwerk2018.Models;
 
 namespace Eindwerk2018.ViewModels
 {
     public class SearchKabelViewModel
     {
+        public Kabel Kabel { get; set; }
     }
 }
