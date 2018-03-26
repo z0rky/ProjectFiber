84efd5f07d5f39d21fc77ad99343c2c60e40fec3
Unknown <koenmeyers81@gmail.com>
2018-03-26 15:34:30 +0200

    Pdf OK layout nog bepalen en implementeren

---
+++ Eindwerk2018/Controllers/PdfController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Eindwerk2018.Models;
+using Eindwerk2018.Reports;
+
+namespace Eindwerk2018.Controllers
+{
+    public class PdfController : Controller
+    {
+        // GET: Pdf
+        public ActionResult Report(Foid foid)
+        {
+            PdfReports pdfReports = new PdfReports();
+            byte[] abytes = pdfReports.PrepareReport(GetFoids());
+            return File(abytes, "application/pdf");
+        }
+
+        public List<Foid> GetFoids()
+        {
+            List<Foid> foidsList = new List<Foid>();
+            // get foids to print
+
+            Foid foid = new Foid();
+
+            foid.Id = 1;
+            foid.Name = "testFoid";
+            foid.Status = 2;
+            foidsList.Add(foid);
+
+
+            return foidsList;
+
+        }
+    }
+}
---
+++ Eindwerk2018/Reports/PdfReports.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Eindwerk2018.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Eindwerk2018.Reports
+{
+    public class PdfReports
+    {
+        #region Declaration
+        int _totalColumn = 3;
+        Document _document;
+        Font _fontStyle;
+        PdfPTable _pdfPTable = new PdfPTable(3);
+        PdfPCell _pdfPCell;
+        MemoryStream _memoryStream = new MemoryStream();
+        List<Foid> _foids = new List<Foid>();
+        #endregion
+
+        public byte[] PrepareReport(List<Foid> foids)
+        {
+            _foids = foids;
+
+            #region
+            _document = new Document(PageSize.A4, 0f,0f,0f,0f);
+            _document.SetPageSize(PageSize.A4);
+            _document.SetMargins(20f, 20f, 20f, 20f);
+            _pdfPTable.WidthPercentage = 100;
+            _pdfPTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _fontStyle = FontFactory.GetFont("Arial", 8f, 1);
+            PdfWriter.GetInstance(_document, _memoryStream);
+            _document.Open();
+            _pdfPTable.SetWidths(new float[]{20f,150f,100f});
+            #endregion
+
+            this.ReportHeader();
+            this.ReportBody();
+            _pdfPTable.HeaderRows = 2;
+            _document.Add(_pdfPTable);
+            _document.Close();
+            return _memoryStream.ToArray();
+        }
+
+        
+
+        private void ReportHeader()
+        {
+            _fontStyle = FontFactory.GetFont("Arial", 11f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("test-test-test", _fontStyle));
+            _pdfPCell.Colspan = _totalColumn;
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.Border = 0;
+            _pdfPCell.BackgroundColor = BaseColor.DARK_GRAY;
+            _pdfPCell.ExtraParagraphSpace = 0;
+            _pdfPTable.AddCell(_pdfPCell);
+            _pdfPTable.CompleteRow();
+
+            _fontStyle = FontFactory.GetFont("Arial", 11f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("test2-test2-test2", _fontStyle));
+            _pdfPCell.Colspan = _totalColumn;
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.Border = 0;
+            _pdfPCell.BackgroundColor = BaseColor.DARK_GRAY;
+            _pdfPCell.ExtraParagraphSpace = 0;
+            _pdfPTable.AddCell(_pdfPCell);
+            // _pdfPTable.CompleteRow();
+
+        }
+
+        private void ReportBody()
+        {
+            #region Table header
+            _fontStyle = FontFactory.GetFont("Tahoma", 9f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("test3-test3-test3", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPTable.AddCell(_pdfPCell);
+
+            
+            _pdfPCell = new PdfPCell(new Phrase("test4-test4-test4", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPTable.AddCell(_pdfPCell);
+
+            
+            _pdfPCell = new PdfPCell(new Phrase("test5-test5-test5", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPTable.AddCell(_pdfPCell);
+            _pdfPTable.CompleteRow();
+
+            #endregion
+
+            #region Table Body
+            _fontStyle = FontFactory.GetFont("Tahoma", 9f, 1);
+            int serialNumber = 1;
+            foreach (Foid foid in _foids )
+            {
+                _pdfPCell = new PdfPCell(new Phrase(serialNumber++.ToString(), _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfPCell.BackgroundColor = BaseColor.WHITE;
+                _pdfPTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(foid.Name, _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfPCell.BackgroundColor = BaseColor.WHITE;
+                _pdfPTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(foid.Status.ToString(), _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfPCell.BackgroundColor = BaseColor.WHITE;
+                _pdfPTable.AddCell(_pdfPCell);
+                _pdfPTable.CompleteRow();
+            }
+
+            #endregion
+        }
+    }
+}
